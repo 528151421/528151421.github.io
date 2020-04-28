230e1efb7bbeaf0abc8194ac8a5fd265039a2c5b
gary <yjt@qq.com>
2020-04-28 16:23:16 +0800

    add file

---
+++ lodash/gary_jiangtao-lodash.js
@@ -0,0 +1,23 @@
+var gary_jingtao = {
+    /**
+     * 
+     * @param {Array} ary 
+     * @param {Number} size
+     * @returns{Array[][]} 
+     */
+    chunk : function (ary,size = 1) {
+        var count = 0;
+        var newAry= [];
+        var temp = [];
+        for(var i = 0; i <ary.length;i++){
+            count++
+            temp.push(ary[i])
+            if(count == size){
+                newAry.push(temp)
+                temp = []
+                count = 0
+            }
+        }
+        return newAry
+    }
+}
