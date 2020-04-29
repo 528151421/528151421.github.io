2fc06a3fa5df2db3c2c84f448ea12b46c528258c
gary <yjt@qq.com>
2020-04-30 01:54:39 +0800

    add

--- lodash/jiangtao159-lodash.js
+++ lodash/jiangtao159-lodash.js
@@ -66,7 +66,7 @@ var jiangtao159 = {
      * @param {Array} ary 
      * @param {...Array}
      */
-    ifference : function(ary){
+    difference : function(ary){
         var res = [];
         var args = Array.from(arguments)
         for(var i = 0; i < ary.length;i++){
@@ -268,5 +268,165 @@ var jiangtao159 = {
             }
         }
         return -1
-    }
+    },
+
+    /**
+     * 
+     * @param {Array} array 
+     * @param {Number} n
+     * @returns {value} 
+     */
+    nth : function(array,n = 0){
+        if(n < 0){
+            return array[array.length + n]
+        }else{
+            return array[n]
+        }
+    },
+
+    pull : function(array){
+        var args = Array.from(arguments);
+        args = args.slice(1)
+        var j = 0;
+        for(var i = 0; i < array.length;i++){
+            if(args.indexOf(array[i]) == -1){
+                array[j] = array[i]
+                j++
+            }
+        }
+        array.length = j
+        return array
+    },
+    /**
+     * 
+     * @param {Array} array 
+     * @param {Array} values
+     * @return {Array} 
+     */
+    pullAll : function(array, values){
+        var j = 0;
+        for(var i = 0; i < array.length;i++){
+            if(values.indexOf(array[i] ) == -1){
+                array[j] = array[i]
+                j++
+            }
+        }
+        array.length = j
+        return array
+    },
+
+    /**
+     * 
+     * @param {Array} array
+     * @returns {Array}
+     *  
+     */
+    reverse : function(array){
+        let i = 0,j = array.length - 1
+        while(i < j){
+            let temp = array[i]
+            array[i] = array[j]
+            array[j] = temp;
+            i++
+            j--
+        }
+        return array
+    },
+
+    /**
+     * 
+     * @param {Array} array 
+     * @param {Number} start 
+     * @param {Number} end
+     * @returns {Array} 
+     */
+    slice : function(array, start=0, end=array.length){
+        let res = [];
+        for(var i = start;i < end;i++){
+            res.push(array[i])
+        }
+        return res
+    },
+
+    /**
+     * 
+     * @param {Array} array 
+     * @param {Number} value
+     * @return {Number} 
+     */
+    sortedIndex : function(array, value){
+        let h = array.length;
+        let l = 0;
+        if(value > array[h - 1]){
+            return array.length
+          }
+          if(value <= array[l]){
+            return 0
+          }
+        while(h > l){
+            let mid = ((h - l) >> 1) + l;
+            if(array[mid] >= value){
+                h = mid;
+            }else if(array[mid] < value && array[mid + 1] >= value){
+                return mid + 1
+            }else{
+                l = mid;
+            }
+        }
+    },
+
+    /**
+     * 
+     * @param {Array} array 
+     * @param {Number} value
+     * @returns {Number} 
+     */
+    sortedLastIndexOf : function(array, value){
+        if(value > array.length - 1 || value < array[0]){
+            return -1
+        }
+        if(value == array[0]){
+            return 0
+        }
+        let low = 0;
+        let height = array.length - 1;
+        while(low < height){
+            let mid = Math.floor((height - low) / 2) + low;
+            if(array[mid] == value && array[mid] > array[mid - 1]){
+                return mid;
+            }else if(array[mid] <= value){
+                height = mid;
+            }else{
+                low = mid;
+            }
+        }
+        return -1
+    },
+
+    /**
+     * 
+     * @param {Array} array 
+     * @param {Number} value
+     * @return {Number} 
+     */
+    sortedLastIndex : function(array, value){
+        let low = 0;
+        let height = array.length - 1;
+        if(value >= array[height]){
+            return array.length
+        }
+        if(value < array[0]){
+            return 0
+        }
+        while(low < height){
+            let mid = Math.floor((height - low) / 2) + low;
+            if(array[mid] < array[mid + 1] && array[mid] >= value){
+                return mid + 1
+            }else if(array[mid] <= value){
+                low = mid;
+            }else{
+                height = mid;
+            }
+        }
+    },
 }
